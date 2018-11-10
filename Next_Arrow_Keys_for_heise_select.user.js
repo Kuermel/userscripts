49237fab572d157d375f541b1cfac4fb81c4c63e
Thomas G <Kuermel@users.noreply.github.com>
2018-11-10 15:30:09 +0100

    Add smooth scrolling

--- Next_Arrow_Keys_for_heise_select.user.js
+++ Next_Arrow_Keys_for_heise_select.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Next/Forward Page for heise select
-// @version      0.2
+// @version      0.3
 // @description  Press Left/Right arrow key for previous/next page
 // @author       Kuermel
 // @match        https://www.heise.de/select/*
@@ -19,6 +19,22 @@ function checkKey(e) {
     else if (e.keyCode == '39' || e.keyCode == '68') {
 	document.getElementsByClassName("nav__link nav__link--next")[0].click();
        // right arrow or d
+    }
+    else if (e.keyCode == '87') {
+        window.scrollBy({
+            "behavior": "smooth",
+            "left": 0,
+            "top": -50
+        });
+       // w
+    }
+    else if (e.keyCode == '83') {
+        window.scrollBy({
+            "behavior": "smooth",
+            "left": 0,
+            "top": 50
+        });
+       // s
     }
 
 }
