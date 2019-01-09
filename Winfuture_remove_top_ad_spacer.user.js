fca7bdbc57474a7986b11bfc5cde9d26fbc6b1eb
Thomas G <Kuermel@users.noreply.github.com>
2019-01-09 01:02:11 +0100

    Update Winfuture_remove_top_ad_spacer.user.js

--- Winfuture_remove_top_ad_spacer.user.js
+++ Winfuture_remove_top_ad_spacer.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name        Winfuture remove top ad spacer
-// @version     0.1
+// @version     0.2
 // @description Winfuture remove top ad spacer
 // @author      Kuermel
 // @match     http://*winfuture.de/*
+// @match     https://*winfuture.de/*
 // ==/UserScript==
 
 document.getElementById("container").style.margin="0px auto auto";
