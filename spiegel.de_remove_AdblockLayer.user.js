21d2f52a8aa82a0871785b11b5f6a4d2ceb818b5
Thomas G <Kuermel@users.noreply.github.com>
2020-06-19 19:35:50 +0200

    Create spiegel.de_remove_AdblockLayer.user.js

---
+++ spiegel.de_remove_AdblockLayer.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         spiegel.de Remove AdblockLayer
+// @version      0.1
+// @author       Kuermel
+// @include      https://www.spiegel.de/*
+// ==/UserScript==
+
+(function() {
+    function remove_lock_layer(){
+    'use strict';
+
+  document.body.style.overflowY = "visible";
+    var elements = document.querySelectorAll("[data-component="AdblockLayer"]");
+        while(elements.length > 0){
+             elements[0].parentNode.removeChild(elements[0]);
+        }
+
+//setTimeout(remove_lock_layer, 5000);
+
+}
+setTimeout(remove_lock_layer, 5000);
+// remove_lock_layer();
+}());
